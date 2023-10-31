fc760b773cc06a9c65889917d83a8106b66e55c7
Nathaniel D. Liebhart <nathaniel95975@gmail.com>
2023-10-31 15:56:01 -0500

    Chat start

--- client/src/components/Chat/ChatHeader.jsx
+++ client/src/components/Chat/ChatHeader.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React from "react"
+import Avatar from "../common/Avatar"
+import { MdCall } from "react-icons/md"
+import { IoVideocam } from "react-icons/io5"
+import { BiSearchAlt2 } from "react-icons/bi"
+import { BsThreeDotsVertical } from "react-icons/bs"
 
 function ChatHeader() {
-  return <div>ChatHeader</div>;
+  return (
+    <div className='h-16 px-4 py-3 flex justify-between items-center bg-panel-header-background z-10'>
+      <div className='flex items-center justify-center gap-6'>
+        <Avatar type='sm' image={"/profile"} />
+        <div className='flex flex-col'>
+          <span className='text-primary-strong'>DEMO</span>
+          <span className='text-secondary text-sm'>online/offline</span>
+        </div>
+      </div>
+      <div className='flex gap-6'>
+        <MdCall className='text-panel-header-icon cursor-pointer text-lx' />
+        <IoVideocam className='text-panel-header-icon cursor-pointer text-lx' />
+        <BiSearchAlt2 className='text-panel-header-icon cursor-pointer text-lx' />
+        <BsThreeDotsVertical className='text-panel-header-icon cursor-pointer text-lx' />
+      </div>
+    </div>
+  )
 }
 
-export default ChatHeader;
+export default ChatHeader
