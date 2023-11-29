5aa64f64a9674a9139d924ef93b664169f429927
Nathaniel D. Liebhart <nathaniel95975@gmail.com>
2023-11-29 16:43:59 -0600

    Search Messages

--- client/src/components/Chat/ChatHeader.jsx
+++ client/src/components/Chat/ChatHeader.jsx
@@ -5,9 +5,10 @@ import { IoVideocam } from "react-icons/io5"
 import { BiSearchAlt2 } from "react-icons/bi"
 import { BsThreeDotsVertical } from "react-icons/bs"
 import { useStateProvider } from "@/context/StateContext"
+import { reducerCases } from "@/context/constants"
 
 function ChatHeader() {
-  const [{ currentChatUser }] = useStateProvider()
+  const [{ currentChatUser }, dispatch] = useStateProvider()
   return (
     <div className='h-16 px-4 py-3 flex justify-between items-center bg-panel-header-background z-10'>
       <div className='flex items-center justify-center gap-6'>
@@ -20,7 +21,10 @@ function ChatHeader() {
       <div className='flex gap-6'>
         <MdCall className='text-panel-header-icon cursor-pointer text-lx' />
         <IoVideocam className='text-panel-header-icon cursor-pointer text-lx' />
-        <BiSearchAlt2 className='text-panel-header-icon cursor-pointer text-lx' />
+        <BiSearchAlt2
+          className='text-panel-header-icon cursor-pointer text-lx'
+          onClick={() => dispatch({ type: reducerCases.SET_MESSAGES_SEARCH })}
+        />
         <BsThreeDotsVertical className='text-panel-header-icon cursor-pointer text-lx' />
       </div>
     </div>
